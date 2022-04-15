550943f15b366c4e86741d1135e736f79013636a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-15 16:58:13 -0500

    Add: task 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/pup
+# This manifests changes details of file
+file { '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
