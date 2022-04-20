d5f9647abff44d85e97cec45f907b5ef706dbcaf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-19 23:55:55 -0500

    Feat(7-puppet_install_nginx_web_server.pp):

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'line':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => ';/server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hellow World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
