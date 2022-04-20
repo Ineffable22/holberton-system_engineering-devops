974bf1de3a9c0d16d7434a098c7dd033c090dd42
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-20 00:01:08 -0500

    Feat(7-puppet_install_nginx_web_server.pp): after was updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ file_line { 'line':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw permanent;',
 }
 
 file { '/var/www/html/index.html':
