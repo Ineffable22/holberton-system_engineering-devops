c32ba6a92c052b80f2c99a55d2c67f6a3e01fabf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-25 22:03:58 -0500

    Feat(2-puppet_custom_http_response_header.pp):

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,46 @@
+# Add a custom HTTP header with Puppet
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+exec { 'apt-get-upgrade':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-upgrade'],
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw permanent;',
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server;',
+  line   => "add_header X-Served-By ${HOSTNAME};",
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
