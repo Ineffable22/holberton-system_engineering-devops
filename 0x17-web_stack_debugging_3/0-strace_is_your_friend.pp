8281038afbb8a7507fa5a7a1a9290bbe204a0467
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-01 21:23:33 -0500

    Feat(0-strace_is_your_friend.pp): provider was removed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Repair error of configuration
 exec { 'repair_server':
-  provider => shell,
-  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  command  => 'sudo sed -i "s/class-wp-error.phpp/class-wp-error.php/g" /var/www/html/wp-settings.php',
+  command => 'sudo sed -i "s/class-wp-error.phpp/class-wp-error.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
