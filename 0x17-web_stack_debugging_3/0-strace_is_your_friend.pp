38072079efc618571884977cc75eee5755950bf7
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-01 21:26:11 -0500

    Feat(0-strace_is_your_friend.pp): code was updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Repair error of configuration
 exec { 'repair_server':
-  command => 'sudo sed -i "s/class-wp-error.phpp/class-wp-error.php/g" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
