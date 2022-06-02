9772875ffcbf9854263fe3b0f7ccb2e64a2397da
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-01 21:15:15 -0500

    Feat(0-strace_is_your_friend.pp): apache restart was removed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,10 +2,4 @@
 exec { 'repair_server':
   provider => shell,
   command  => 'sed -i "s/class-wp-error.phpp/class-wp-error.php/g" /var/www/html/wp-settings.php',
-  before   => Exec['restart_apache'],
-}
-
-exec { 'restart_apache':
-  provider => shell,
-  command  => 'sudo service apache2 restart',
 }
